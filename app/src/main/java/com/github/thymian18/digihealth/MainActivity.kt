d369ac12ce6761965c3212041b5e5400c0259bb1
Thymian18 <damian.kopp01@gmail.com>
2023-08-31 10:57:07 +0200

    Adapt the non-main activities to have static coloring

--- app/src/main/java/com/github/thymian18/digihealth/MainActivity.kt
+++ app/src/main/java/com/github/thymian18/digihealth/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -141,7 +140,6 @@ fun WelcomeTextRow() {
             textAlign = TextAlign.Center,
         )
     }
-
 }
 
 
--- app/src/main/java/com/github/thymian18/digihealth/SettingsActivity.kt
+++ app/src/main/java/com/github/thymian18/digihealth/SettingsActivity.kt
@@ -18,7 +18,7 @@ class SettingsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DigihealthTheme {
+            DigihealthTheme(dynamicColor = false) {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
